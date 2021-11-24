085eda27de44f5b26d358bae0b6474be63961e8e
demonick82 <demonick82@gmail.com>
2021-11-25 02:17:17 +0300

    test commit

---
+++ src/main/java/toone/models/Role.java
@@ -0,0 +1,63 @@
+package toone.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity
+@Table(name = "j_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public static Role of(String name) {
+        Role role = new Role();
+        role.name = name;
+        return role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role role = (Role) o;
+        return id == role.id &&
+                Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Role.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .toString();
+    }
+}
